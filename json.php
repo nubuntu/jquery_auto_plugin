c82c9dd80d75eeb8d4f8930000031c049b284534
Nurkholis <poetnoer@gmail.com>
2015-10-04 00:50:01 +0700

    clean get post

--- json.php
+++ json.php
@@ -4,10 +4,27 @@ if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
 }
 
-switch($_GET['autotable_action']){
+$post   = $_POST;
+$get    = $_GET;
+
+# clean post
+if(count($post)>0){
+    foreach($post as $key => $value){
+        $post[$key] = $mysqli->real_escape_string($value);    
+    }
+}
+
+# clean get
+if(count($get)>0){
+    foreach($get as $key => $value){
+        $get[$key] = $mysqli->real_escape_string($value);    
+    }
+}
+
+switch($get['autotable_action']){
     case 'get_records':
-        $page               = $_GET['autotable_page'];
-        $limit              = $_GET['autotable_limit'];
+        $page               = $get['autotable_page'];
+        $limit              = $get['autotable_limit'];
         $start              = ($page-1) * $limit;
         $q                  = "select * from tbl_students limit $start,$limit";
         $data['records']    = $mysqli->query($q)->fetch_all(MYSQLI_ASSOC);
@@ -15,25 +32,25 @@ switch($_GET['autotable_action']){
         die(json_encode($data));
     break;
     case 'form_save':
-        if($_POST['id']==null){
+        if($post['id']==null){
             # new record
             $q      = 'insert into tbl_students(name,city,date_of_birth,register_date,status) 
-                    values ("'.$_POST['name'].'","'.$_POST['city'].'","'.$_POST['date_of_birth'].'","'.$_POST['register_date'].'","'.$_POST['status'].'")';
+                    values ("'.$post['name'].'","'.$post['city'].'","'.$post['date_of_birth'].'","'.$post['register_date'].'","'.$post['status'].'")';
             $result = $mysqli->query($q);
             die($result);                     
         }else{
             # edit record
-            $q      = 'update tbl_students set name="'.$_POST['name'].'",city="'.$_POST['city'].'",date_of_birth="'.$_POST['date_of_birth'].'",
-                        register_date="'.$_POST['register_date'].'",status="'.$_POST['status'].'" 
-                        where id="'.$_POST['id'].'"'; 
+            $q      = 'update tbl_students set name="'.$post['name'].'",city="'.$post['city'].'",date_of_birth="'.$post['date_of_birth'].'",
+                        register_date="'.$post['register_date'].'",status="'.$post['status'].'" 
+                        where id="'.$post['id'].'"'; 
             $result = $mysqli->query($q);
             die($result);                     
         }
     break;    
     case 'form_delete':
-        if($_POST['id']!=null){
+        if($post['id']!=null){
             # new record
-            $q      = 'delete from tbl_students where id='.$_POST['id']; 
+            $q      = 'delete from tbl_students where id='.$post['id']; 
             $result = $mysqli->query($q);
             die($result);                     
         }else{
